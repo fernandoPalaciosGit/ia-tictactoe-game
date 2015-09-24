b57f631be38931d3b060925e3353e7e1e07f3968
fernandoPalaciosGit <fernandopalacioslandi@gmail.com>
2015-09-24 22:14:49 +0200

    matrix structure in JS, grid 3x3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('init');
+;(function () {
+    'use strict';
+
+    var matrix = {
+        grid: [],
+        class: 'js-matrix-',
+        columns: 3,
+        rows: 3
+    };
+
+    // coordenates : js-matrix-{column}-{row}
+    for (var i = 0; i < matrix.rows; i++) {
+        matrix.grid.push(new Array(matrix.columns));
+
+        for (var j = 0; j < matrix.columns; j++) {
+            matrix.grid[i][j] = matrix.class + j + i;
+        }
+    }
+
+    console.dir(matrix.grid);
+}(window, document));
