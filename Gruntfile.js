4ffbd32d69ca0d6523f54fabd3e9c66167a390c4
fernandoPalaciosGit <fernandopalacioslandi@gmail.com>
2015-10-12 00:03:57 +0200

    git hook to throw grunt linters task on push to any remote repository

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,5 +48,5 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('default', ['sass:dist', 'jshint', 'jscs', 'eslint']);
+    grunt.registerTask('preci', ['sass:dist', 'jshint', 'jscs', 'eslint']);
 };
